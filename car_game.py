840a886dd490d7072996729b0a7aab8f8447d06b
MartaKasprzyk <martha.kasprzyk@gmail.com>
2024-02-16 20:50:25 +0100

    prepared the graphic representation of the road

--- car_game.py
+++ car_game.py
@@ -1,9 +1,21 @@
 import pygame
 from pygame.locals import *
 
+size = width, height = (800, 800)
+road_width = int(width/1.5)
+roadmark = int(width/80)
+
 pygame.init()
 running = True
-screen = pygame.display.set_mode((800, 800))
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("MEK's car game")
+screen.fill((128, 255, 128))
+pygame.draw.rect(screen, (50, 50, 50), (width/2-road_width/2, 0, road_width, height))
+pygame.draw.rect(screen, (255, 255, 255), (width/2-roadmark/2, 0, roadmark, height))
+pygame.draw.rect(screen, (255, 255, 255), (width/2-road_width/2 + roadmark*2, 0, roadmark, height))
+pygame.draw.rect(screen, (255, 255, 255), (width/2+road_width/2 - roadmark*3, 0, roadmark, height))
+
+pygame.display.update()
 
 while running:
     for event in pygame.event.get():
